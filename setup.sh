68da7c8a258baa79d46814d2fbf7baf3854e309e
Silver Nunez <silvestrejna@hotmail.com>
2024-06-25 17:47:41 -0600

    Update setup.sh instructions

--- setup.sh
+++ setup.sh
@@ -1,16 +1,17 @@
 # !/bin/bash
 
+#Environment variables
 export PROJECT_ID="active-dahlia-424818-m5"
 export REGION="us-east1"
 export DOMAIN="demobank.silver-en-irlanda.com"
 
-
+# Terraform state folder
 gsutil mb gs://${PROJECT_ID}-boa-tf-state
 gsutil versioning set on gs://${PROJECT_ID}-boa-tf-state
 
-run from iac/tf-multienv-cicd-anthos-autopilot
-   terraform init && \
-   terraform apply
+# Terraform infrastructure
+terraform -chdir=iac/tf-multienv-cicd-anthos-autopilot init
+terraform -chdir=iac/tf-multienv-cicd-anthos-autopilot apply
 
 gcloud container fleet mesh describe
 
